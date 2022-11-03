bc786e9975d6ec28f5ff46d08b63fa207fd60ca2
meganmcadams <megmcmuffin14@gmail.com>
2022-11-02 19:43:22 -0500

    fixed bugs for bubble sort, displays results

--- Scripts/BubbleSort.js
+++ Scripts/BubbleSort.js
@@ -2,24 +2,37 @@
 // comes from https://www.geeksforgeeks.org/bubble-sort/
 
 const btn = document.getElementById('run_algorithm');
-const result_box = document.getElementById('results');
+const result_input = document.getElementById('result_input');
+const result_box = document.getElementById('result_box');
 
 btn.addEventListener('click', function onClick(event) {
 
-    arr = document.getElementById("run_algorithm").value;
-    
-    result = convertArrayToDisplay(bubbleSort(arr));
-    result_box.innerHTML = result;
+    arr = convertToArray(result_input.value);
+    passes = convertArrayToDisplay(bubbleSort(arr));
+    result_box.innerHTML = passes;
 
   
 });
 
+function convertToArray(arr){
+
+    arr = arr.split(',');
+    let size = arr.length;
+    for(let i = 0; i < size; i++){
+
+        arr[i] = parseFloat(arr[i]);
+
+    }
+
+    return arr;
+
+}
+
 function bubbleSort(arr) {
 
-    arr = arr.split(','); // arr to sort
-    let passes = [arr[0]]; // initialize passes array with the original arr
+    let passes = [[...arr]]; // initialize passes array with the original arr
 
-    const size = length(arr);
+    const size = arr.length;
     let i = 0;
     let temp = 0;
     let swapped = false;
@@ -37,12 +50,14 @@ function bubbleSort(arr) {
 
                 swapped = true; // indicate that elements were swapped
 
-            }
+            } i += 1;
 
-        } passes.push(arr); // add current array to passes array
+        } passes.push([...arr]); // add a copy of the current array to passes array
 
         if(!swapped){ break; }
         else { swapped = false; }
+
+        i = 0;
 
     }
 
@@ -50,20 +65,21 @@ function bubbleSort(arr) {
 
 }
 
-function convertArrayToDisplay(arr){
+function convertArrayToDisplay(passes){
 
     let result = "";
-    let size = length(arr);
-    let element_size = length(arr[0]);
+    let size = passes.length;
+    let element_size = passes[0].length;
 
-    for(let i = 0; i < size; i++){ // for each pass
+    for (let i = 0; i < size; i++) { // for each pass
 
         result += "Pass " + i.toString() + ": "; // add the pass #
         for(let j = 0; j < element_size; j++){ // for each element in the current pass
 
-            result += arr[j].toString(); // add the current element
-            if(j < size - 1){ result += ", "; } // if is not the end, add a comma
-            else { result += "\n"; } // if it is the end, add a newline
+            result += passes[i][j].toString(); // add the current element
+            console.log(passes[i],"is current");
+            if (j < element_size - 1) { result += ", "; } // if is not the end, add a comma
+            else { result += "<br>"; } // if it is the end, add a newline
 
         }
 
